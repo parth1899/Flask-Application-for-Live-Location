0bbb17d0730873b9dfeb0568cb732c2b12da6f4a
Parth Kalani <parthkalani1899@gmail.com>
2024-05-19 14:49:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/live-coordinates')
+def live_coordinates():
+    # Simulate live coordinates for demonstration
+    lat = 18.5434664
+    lng = 73.7831388
+    return jsonify(lat=lat, lng=lng)
+
+if __name__ == '__main__':
+    app.run(debug=True)
